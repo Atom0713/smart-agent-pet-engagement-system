7faa596e5eed8a61f182ae40e74feb7a49bcf605
Artem Sliusarenko <artemsliusarenko@Artems-MacBook-Pro.local>
2024-04-20 16:52:20 +0200

    fetch and persist weather report at the tie of sesor activation skeleton

--- components/data_aggregation/src/app.py
+++ components/data_aggregation/src/app.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from .database.table import initialize_db
 from .models import SensorActivations
 from .schemas import HttpResponse, SensorInput
+from components.data_aggregation.src import smhi
+import json
 
 app = FastAPI()
 
@@ -16,5 +18,8 @@ async def read_root() -> HttpResponse:
 
 @app.post("/collect")
 async def collect(sensor_input: SensorInput) -> HttpResponse:
-    SensorActivations({"activated_at": sensor_input.activated_at}).save()
+    weather_report: str = await smhi.get(sensor_input.lon, sensor_input.lat)
+    SensorActivations(
+        {"activated_at": sensor_input.activated_at, "weather_report": weather_report}
+    ).save()
     return HttpResponse()
---
+++ components/data_aggregation/src/smhi.py
@@ -0,0 +1,17 @@
+from aiohttp import ClientSession
+import json
+
+BASE_URL: str = (
+    "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/{}/lat/{}/data.json"
+)
+
+
+async def get(lon: int, lat: int) -> str:
+    async with ClientSession() as session:
+        response = await _get(session, BASE_URL.format(lon, lat))
+        return response
+
+
+async def _get(session: ClientSession, url: str) -> str:
+    async with session.get(url) as response:
+        return await response.text()
