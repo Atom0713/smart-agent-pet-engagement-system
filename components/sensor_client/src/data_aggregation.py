3884a178ef3d3b4399545f232a3e30e5acf7169f
Artem Sliusarenko <artemsliusarenko@Artems-MacBook-Pro.local>
2024-05-05 15:11:53 +0200

    post sensor location data

--- components/sensor_client/src/data_aggregation.py
+++ components/sensor_client/src/data_aggregation.py
@@ -1,8 +1,16 @@
 import requests
+from requests.exceptions import ConnectionError
+from logger import logger
+import asyncio
 
 BASE_URL: str = "http://127.0.0.1:8004"
 
 
 async def post(url: str, data: dict) -> None:
     url = f"{BASE_URL}{url}"
-    _ = requests.post(url, json=data)
+    try:
+        _ = requests.post(url, json=data)
+    except ConnectionError as e:
+        logger.info(f"Data aggregation server is offline! Wait.")
+        await asyncio.sleep(60)
+        
--- components/sensor_client/src/sensor_client.py
+++ components/sensor_client/src/sensor_client.py
@@ -1,4 +1,5 @@
 import asyncio
+from enum import Enum
 import logging
 from datetime import datetime
 
@@ -8,9 +9,12 @@ from loop import run
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
+class Location(Enum):
+    LIVING_ROOM = 1
 
-async def post_sensor_activation_report(now: datetime, lat: int, lon: int) -> None:
-    data: dict = {"activated_at": now.strftime("%m/%d/%Y, %H:%M:%S"), "lon": lon, "lat": lat}
+
+async def post_sensor_activation_report(now: datetime, lat: int, lon: int, location: int) -> None:
+    data: dict = {"activated_at": now.strftime("%m/%d/%Y, %H:%M:%S"), "lon": lon, "lat": lat, "location": location}
     await data_aggregation.post("/collect", data)
 
 
@@ -18,8 +22,9 @@ async def sensor_client() -> None:
     now: datetime = datetime.now()
     lat: int = 59
     lon: int = 17
-    logger.info(f"Motion sensor activated. Timestamp: {now}. Lattitude: {lat}, Longitude: {lon}")
-    await post_sensor_activation_report(now, lat, lon)
+    location: int = Location.LIVING_ROOM.value
+    logger.info(f"Motion sensor activated. Timestamp: {now}. Lattitude: {lat}, Longitude: {lon}, Location: {location}")
+    await post_sensor_activation_report(now, lat, lon, location)
     await asyncio.sleep(20)
 
 
