5fde7ad39a68d7a6ea254fa47795b3115f48ff9f
Artem Sliusarenko <artemsliusarenko@Artems-MacBook-Pro.local>
2024-05-05 15:29:32 +0200

    code formatting

--- components/data_processing/src/models/sensor_activations.py
+++ components/data_processing/src/models/sensor_activations.py
@@ -8,9 +8,10 @@ class SensorActivations:
     table = ddb_client.Table(TableName)
     PKAttributeName: str = "pk"
 
-    async def get_item(self, low_value: str, high_value: str) -> dict:
+    async def get_item(self, low_value: str, high_value: str, location: str) -> dict:
         response = self.table.query(
-            KeyConditionExpression=Key("pk").eq("MOTION_SENSOR") & Key("activated_at").between(low_value, high_value),
+            KeyConditionExpression=Key("pk").eq(f"MOTION_SENSOR_{location}")
+            & Key("activated_at").between(low_value, high_value),
             Limit=20,
         )
         return response
